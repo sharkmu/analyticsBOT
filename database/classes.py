fd97fc6fb8e80d2a0b39db1f2506546569191fc4
decaplanet <contact@decaplanet.com>
2023-11-13 04:31:00 +0900

    feat: implement more functions
    get_guild, add_or_update_guild

---
+++ database/classes.py
@@ -0,0 +1,132 @@
+import sqlite3
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class UserData:
+    user_id: str
+    chat_count: int
+
+
+@dataclass
+class GuildData:
+    member_count: int
+    user_ban_count: int
+    chat_count: int
+
+
+@dataclass
+class Guild:
+    guild_id: str
+    guild_data: GuildData
+    user_data: list[UserData]
+
+
+class Database:
+    def __init__(self, db_name: str | None, temp_db_name: str = "bot_test_db"):
+        cwd = os.getcwd()
+
+        if db_name == None or db_name == "":
+            self.db_path = f"{cwd}/database/db/{temp_db_name}"
+        else:
+            self.db_path = f"{cwd}/database/db/{db_name}"
+
+        self.get_connection = lambda: sqlite3.connect(self.db_path)
+
+
+class DiscordBotDatabase(Database):
+    def __init__(self, db_name: str = None):
+        super().__init__(db_name)
+
+        con = self.get_connection()
+        cur = con.cursor()
+
+        guild = cur.execute("""
+            CREATE TABLE IF NOT EXISTS guilds (
+                guild_id TEXT PRIMARY KEY, 
+                member_count INTEGER, 
+                user_ban_count INTEGER, 
+                chat_count INTEGER
+            )
+""")
+        users = cur.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+                guild_id TEXT, 
+                user_id TEXT, 
+                chat_count INTEGER, 
+                PRIMARY KEY (guild_id, user_id)
+            )
+        """)
+
+        con.commit()
+        con.close()
+
+        if guild == None or users == None:
+            raise Exception("Could not complete database setup.")
+
+    def get_guild(self, guild_id: str) -> Guild:
+        con = self.get_connection()
+        cur = con.cursor()
+
+        guild = cur.execute(
+            "SELECT * FROM guilds WHERE guild_id = ?", (guild_id,)).fetchone()
+        users = cur.execute(
+            "SELECT * FROM users WHERE guild_id = ?", (guild_id,)).fetchmany()
+
+        con.commit()
+        con.close()
+
+        if guild != None:
+            guild_data = Guild(
+                guild_id=guild[0],
+                guild_data=GuildData(
+                    member_count=guild[1],
+                    user_ban_count=guild[2],
+                    chat_count=guild[3]
+                ),
+                user_data=users
+            )
+
+            return guild_data
+        else:
+            return None
+
+    def add_or_update_guild(self, guild_id: str, guild_data: GuildData):
+        con = self.get_connection()
+        cur = con.cursor()
+
+        existing_guild = self.get_guild(guild_id)
+
+        # Update guild data if it already exists
+        # Otherwise, add one.
+        if existing_guild != None and existing_guild.guild_data != guild_data:
+            existing_guild.guild_data = guild_data
+        elif existing_guild == None:
+            existing_guild = cur.execute(
+                "INSERT INTO guilds VALUES (?, ?, ?, ?)", (guild_id, 0, 0, 0,)).fetchone()
+
+        con.commit()
+        con.close()
+
+        # Return added or updated guild
+        if existing_guild != None:
+            returning_guild = Guild(
+                guild_id=existing_guild.guild_id,
+                guild_data=GuildData(
+                    member_count=existing_guild.guild_data.member_count,
+                    user_ban_count=existing_guild.guild_data.user_ban_count,
+                    chat_count=existing_guild.guild_data.chat_count
+                ),
+                user_data=[existing_guild.user_data]
+            )
+
+            return returning_guild
+        else:
+            return None
+
+    def remove_guild(guild_id: str):
+        pass
+
+    def add_or_update_user(guild_id: str, user_id: str):
+        pass
---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+from database.classes import GuildData, Guild, DiscordBotDatabase
+
+# Enter following command to run tests:
+# python3 -m unittest
+
+
+class DatabaseTest(unittest.TestCase):
+    def test_add_guild(self):
+        db = DiscordBotDatabase()
+        db.add_or_update_guild(guild_id="1234567890", guild_data=GuildData(
+            member_count=32, user_ban_count=4, chat_count=0))
+
+        self.assertEqual(db.get_guild(guild_id="1234567890"), Guild(guild_id='1234567890', guild_data=GuildData(
+            member_count=0, user_ban_count=0, chat_count=0), user_data=[]))
+
+
+if __name__ == '__main__':
+    unittest.main()
