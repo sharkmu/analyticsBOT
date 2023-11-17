e47ec773ce5e4253a464d82e3e780cff28986cab
decaplanet <contact@decaplanet.com>
2023-11-18 07:33:31 +0900

    chore: delete user database and refactor code

--- database/live_update_database.py
+++ database/live_update_database.py
@@ -4,13 +4,6 @@ from dataclasses import dataclass
 
 
 @dataclass
-class UserData:
-    guild_id: str
-    user_id: str
-    chat_count: int = 0
-
-
-@dataclass
 class GuildData:
     member_count: int
     user_ban_count: int = 0
@@ -21,7 +14,6 @@ class GuildData:
 class Guild:
     guild_id: str
     guild_data: GuildData
-    user_list: list[UserData]
 
 
 class Database:
@@ -45,53 +37,39 @@ class LiveUpdateDatabase(Database):
         con = self.get_connection()
         cur = con.cursor()
 
-        guild = cur.execute("""
-            CREATE TABLE IF NOT EXISTS
-                guilds (
-                    guild_id TEXT PRIMARY KEY, 
-                    member_count INTEGER, 
-                    user_ban_count INTEGER, 
-                    chat_count INTEGER
-                );
-        """)
-        users = cur.execute("""
-            CREATE TABLE IF NOT EXISTS
-                users(
-                    guild_id TEXT, 
-                    user_id TEXT, 
-                    chat_count INTEGER, 
-                    PRIMARY KEY (guild_id, user_id)
-                );
-        """)
+        guild = cur.execute(
+            """
+                CREATE TABLE IF NOT EXISTS
+                    guilds (
+                        guild_id TEXT PRIMARY KEY, 
+                        member_count INTEGER, 
+                        user_ban_count INTEGER, 
+                        chat_count INTEGER
+                    );
+            """
+        )
 
         con.commit()
         con.close()
 
-        if guild == None or users == None:
+        if guild == None:
             raise Exception("Could not complete database setup.")
 
     def get_guild(self, guild_id: str) -> Guild | None:
         con = self.get_connection()
         cur = con.cursor()
 
-        guild = cur.execute("""
-            SELECT
-                *
-            FROM
-                guilds
-            WHERE
-                guild_id = ?;
-        """, (guild_id,)
+        guild = cur.execute(
+            """
+                SELECT
+                    *
+                FROM
+                    guilds
+                WHERE
+                    guild_id = ?;
+            """,
+            (guild_id,)
         ).fetchone()
-        users = cur.execute("""
-            SELECT
-                *
-            FROM
-                users
-            WHERE
-                guild_id = ?;
-        """, (guild_id,)
-        ).fetchall()
 
         con.commit()
         con.close()
@@ -104,50 +82,9 @@ class LiveUpdateDatabase(Database):
                     user_ban_count=guild[2],
                     chat_count=guild[3]
                 ),
-                user_list=[
-                    UserData(
-                        guild_id=user[0],
-                        user_id=user[1],
-                        chat_count=user[2]
-                    ) for user in users
-                ]
             )
 
             return guild_data
-        else:
-            return None
-
-    def get_user(self, guild_id: str, user_id: str) -> UserData | None:
-        con = self.get_connection()
-        cur = con.cursor()
-
-        user = cur.execute(
-            """
-            SELECT
-                *
-            FROM
-                users
-            WHERE
-                guild_id = ?
-                AND user_id = ?;
-            """,
-            (
-                guild_id,
-                user_id,
-            )
-        ).fetchone()
-
-        con.commit()
-        con.close()
-
-        if user != None:
-            user_data = UserData(
-                guild_id=user[0],
-                user_id=user[1],
-                chat_count=user[2]
-            )
-
-            return user_data
         else:
             return None
 
@@ -157,7 +94,8 @@ class LiveUpdateDatabase(Database):
 
         existing_guild = self.get_guild(guild_id)
 
-        # Use default value when neither the argument nor the existing guild data has a value.
+        # Use default value when neither the argument
+        # nor the existing guild data has a value.
         default = GuildData(
             member_count=0,
             user_ban_count=0,
@@ -187,18 +125,19 @@ class LiveUpdateDatabase(Database):
             )
         )
 
-        # Update guild data if the guild already exists. Otherwise, add one.
+        # Update guild data if the guild already exists.
+        # Otherwise, add one.
         if existing_guild != None:
             cur.execute(
                 """
-                UPDATE
-                    guilds
-                SET
-                    member_count = ?,
-                    user_ban_count = ?,
-                    chat_count = ?
-                WHERE
-                    guild_id = ?;
+                    UPDATE
+                        guilds
+                    SET
+                        member_count = ?,
+                        user_ban_count = ?,
+                        chat_count = ?
+                    WHERE
+                        guild_id = ?;
                 """,
                 (
                     using_guild_data.member_count,
@@ -210,71 +149,17 @@ class LiveUpdateDatabase(Database):
         elif existing_guild == None:
             cur.execute(
                 """
-                INSERT INTO
-                    guilds
-                VALUES
-                    (?, ?, ?, ?);
+                    INSERT INTO
+                        guilds
+                    VALUES
+                        (?, ?, ?, ?);
                 """,
                 (
                     guild_id,
                     using_guild_data.member_count,
                     using_guild_data.user_ban_count,
                     using_guild_data.chat_count,
-                ))
-
-        con.commit()
-        con.close()
-
-    def add_or_update_user(self, guild_id: str, user_id: str, user_data_arg: UserData = None):
-        con = self.get_connection()
-        cur = con.cursor()
-
-        existing_user_data = self.get_user(guild_id, user_id)
-
-        # If user data is not provided, use default values.
-        default = UserData(
-            user_id=user_id,
-            guild_id=guild_id,
-            chat_count=0
-        )
-        if existing_user_data:
-            using_user_data = existing_user_data
-        else:
-            using_user_data = default
-
-        using_user_data = UserData(
-            user_id=user_id,
-            guild_id=guild_id,
-            chat_count=getattr(
-                user_data_arg,
-                'chat_count',
-                using_user_data.chat_count
-            )
-        )
-
-        # Update user data if the user already exists. Otherwise, add one.
-        if existing_user_data != None:
-            cur.execute(
-                """
-                UPDATE
-                    users
-                SET
-                    chat_count = ?
-                WHERE
-                    guild_id = ? AND user_id = ?;
-                """,
-                (using_user_data.chat_count, guild_id, user_id,)
-            )
-
-        elif existing_user_data == None:
-            cur.execute(
-                """
-                INSERT INTO
-                    users
-                VALUES
-                    (?, ?, ?);
-                """,
-                (guild_id, user_id, 0,)
+                )
             )
 
         con.commit()
@@ -285,41 +170,13 @@ class LiveUpdateDatabase(Database):
         cur = con.cursor()
 
         cur.execute(
-            "DELETE FROM users WHERE guild_id = ?",
-            (guild_id,)
-        )
-        cur.execute(
             """
-            DELETE FROM
-                guilds
-            WHERE
-                guild_id = ?
+                DELETE FROM
+                    guilds
+                WHERE
+                    guild_id = ?
             """,
             (guild_id,)
-        )
-        deleted_rows = cur.rowcount
-
-        con.commit()
-        con.close()
-
-        if deleted_rows < 1:
-            return False
-
-        return True
-
-    def delete_user(self, guild_id: str, user_id: str) -> bool:
-        con = self.get_connection()
-        cur = con.cursor()
-
-        cur.execute(
-            """
-            DELETE FROM
-                users
-            WHERE
-                guild_id = ?
-                AND user_id = ?
-            """,
-            (guild_id, user_id,)
         )
         deleted_rows = cur.rowcount
 
