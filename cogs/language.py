2cb01f8cda54cbfe2bc00eeb6aa0ef52cd33e50f
Samu <csonka.samuel@gmail.com>
2023-11-27 19:06:40 +0100

    chore: update DB in language cog

--- cogs/language.py
+++ cogs/language.py
@@ -2,17 +2,23 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from typing import Literal
+from database.live_update_database import LiveUpdateDatabase
 
 
 class language(commands.Cog): 
     def __init__(self, bot: commands.Bot): 
         self.bot = bot
+        self.db = LiveUpdateDatabase()
  
     @app_commands.command(name="language", description="Change the language of the bot. Required permission: administrator")
     @app_commands.default_permissions(administrator=True)
-    async def language(self, interaction: discord.Interaction, languages: Literal["English", "Magyar", "Deutsch"]):
-        await interaction.response.send_message(f"You choose this: {languages}")
-
+    async def language(self, interaction: discord.Interaction, languages: Literal["english", "magyar", "deutsch"]):
+        try:
+            self.db.update_language(str(interaction.guild_id), languages)
+            await interaction.response.send_message(f"You successfully updated the language to this: {languages}")
+        except Exception as e:
+            await interaction.response.send_message(f"Something went wrong, try again later! Error: `{e}`")
+        print(self.db.get_language(str(interaction.guild_id)))
     
 
 async def setup(bot: commands.Bot) -> None:
