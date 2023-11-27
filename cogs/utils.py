699cde0297bea3a3e4050d3e004bc64580601898
Samu <csonka.samuel@gmail.com>
2023-11-27 20:30:01 +0100

    complete #18

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,13 +1,29 @@
 from discord.ext import commands
 from config import config
 from database.live_update_database import LiveUpdateDatabase
+from config import config
 
 class utils(commands.Cog): 
     def __init__(self, bot: commands.Bot): 
         self.bot = bot
         self.db = LiveUpdateDatabase()
-
+        
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        lang = self.db.get_language(str(ctx.guild.id))
+        lang_config = getattr(config, lang[0]) #type: ignore
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send(lang_config["CHECK_FAILURE_MSG"])
+        else:
+            pass # TODO
+    
+    def is_me():
+        def predicate(ctx):
+            return ctx.message.author.id == config.OWNER
+        return commands.check(predicate)
+    
     @commands.hybrid_command()
+    @is_me()
     async def sync(self, ctx):
         lang = self.db.get_language(str(ctx.guild.id))
         lang_config = getattr(config, lang[0]) #type: ignore     
@@ -18,6 +34,7 @@ class utils(commands.Cog):
             await ctx.send(lang_config["SYNC_ERROR_MSG"] + str(e))
 
     @commands.hybrid_command()
+    @is_me()
     async def reload(self, ctx, *, cog: str):
         lang = self.db.get_language(str(ctx.guild.id))
         lang_config = getattr(config, lang[0]) #type: ignore     
